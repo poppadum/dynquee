8b309339477e921eb1f51d938bd160500046e434
Chris Laird <chris@pocketluff.net>
2023-01-17 16:30:11 +0000

    can now choose a file at random from matching files

--- es_mqtt_sub.sh
+++ es_mqtt_sub.sh
@@ -31,21 +31,44 @@ shopt -s nullglob
 
 
 # Send timestamped message to logfile
-# Params: text to log
-# DEBUG: log to stdout while developing
+# Params: $@ = text to log
+# DEBUG: for now, log to stderr while developing
 log() {
-	printf "%s: %s\n" "$(date +"$LOGDATEFMT")" "$*"  # >> "$LOGFILE"
+	printf "%s: %s\n" "$(date +"$LOGDATEFMT")" "$*" >&2 #>> "$LOGFILE"
 	# TODO: also log to recallog
 }
+
+
+# Choose a random item from a list
+# Params: $@ = a list
+chooseRandomFrom() {
+	log "chooseRandomFrom $@ ($# items)"
+	# Can't pick from an empty list: exit with code 1
+	if [ $# -eq 0 ]; then
+		return 1
+	fi
+	# Pick a random item and return it
+	items=("$@")
+	numItems=${#items[@]}
+	#echo "numItems=$numItems"
+	randomIndex=$(( $RANDOM % $numItems ))
+	#echo "randomIndex=$randomIndex"
+	echo "${items[$randomIndex]}"
+}
+
+
+#chooseRandomFrom a b c d
+#chooseRandomFrom || echo empty list
+#exit
 
 
 
 # Event handler
 # Params: $1 = Action
-#		 $2 = ActionData
-#		 $3 = SystemId
-#		 $4 = GamePath
-#		 $5 = ImagePath
+#         $2 = ActionData
+#         $3 = SystemId
+#         $4 = GamePath
+#         $5 = ImagePath
 handleEvent() {
 	case "$1" in
 		systembrowsing|gamelistbrowsing)
@@ -73,22 +96,38 @@ handleEvent() {
 
 # Display a still image or video clip on the marquee
 # Params: $1 = path to image/video file
-#		 $2 = additional options to mpv
+#         $2 = additional options to mpv
 showOnMarquee() {
-		# Stop the previous player if one is running
-		if [[ "$playerPid" != "$PLAYER_PID_UNKNOWN" ]]; then
-				kill $playerPid && \
-					playerPid=$PLAYER_PID_UNKNOWN
-		fi
-		# launch player and record pid so we can stop it later
-		printf "DEBUG: run command: %s\n" "$PLAYER $PLAYER_OPTS \"$1\""
-		if $PLAYER $PLAYER_OPTS "$1" &
-		then
-			echo "DEBUG: command exit code $?"
-			playerPid=$!
-		fi
+	# Stop the previous player if one is running
+	if [[ "$playerPid" != "$PLAYER_PID_UNKNOWN" ]]; then
+		kill $playerPid && \
+			playerPid=$PLAYER_PID_UNKNOWN
+	fi
+	# launch player and record pid so we can stop it later
+	printf "DEBUG: run command: %s\n" "$PLAYER $PLAYER_OPTS \"$1\""
+	if $PLAYER $PLAYER_OPTS "$1" &
+	then
+		echo "DEBUG: command exit code $?"
+		playerPid=$!
+	fi
 }
 
+
+
+# Search for ROM-specific media files, and if >1 found, return one at random.
+# If none found, exits with return code 1
+# Params: $1 = SystemId
+#         $2 = gameBasename
+getMarqueeMediaForROM() {
+	# find ROM-specific media files in $MARQUEE_BASE/$SystemId/$gameBasename
+	files=( "$MARQUEE_BASE/$1/$2".* )
+	log "searching for marquee media in '$MARQUEE_BASE/$1/$2.*'"
+	log "found ${#files[@]} files: ${files[@]}"
+	if [[ ${#files[@]} -eq 0 ]]; then
+		return 1
+	fi
+	chooseRandomFrom "$files"
+}
 
 
 # Work out which media file to display on the marquee.
@@ -99,10 +138,10 @@ showOnMarquee() {
 #  4. generic file
 #
 # Params: $1 = Action
-#		 $2 = ActionData
-#		 $3 = SystemId
-#		 $4 = GamePath
-#		 $5 = ImagePath
+#         $2 = ActionData
+#         $3 = SystemId
+#         $4 = GamePath
+#         $5 = ImagePath
 getMarqueeMedia() {
 	# Get game filename without directory and extension
 	gameBasename=$(basename "$4")
@@ -114,19 +153,20 @@ getMarqueeMedia() {
 	log "gameBase=$gameBasename"
 	
 	# ROM-specific media file
-	files=( "$MARQUEE_BASE/$3/$gameBasename".* )
-	log "searching for marquee media in '$MARQUEE_BASE/$3/$gameBasename.*'"
-	log "found ${#files[@]} files: ${files[@]}"
-
+	if file=$( getMarqueeMediaForROM "$3" "$gameBasename" ); rc=$?; [[ $rc -eq 0 ]]; then
+		log "selected ROM-specific media file '$file'"
 	# Scraped image
-	if [[ "$5" != "" ]]; then
-		showOnMarquee "$5"
+	elif  [[ "$5" != "" ]]; then
+		file="$5"
+		log "selected ROM image file '$file'"
+	# last resort: Recalbox logo
+	else
+		file=/recalbox/share_init/system/tft_logos/320p/recalbox.png
 	fi
+
+	# display chosen media
+	showOnMarquee "$file"
 }
-
-
-
-
 
 
 
@@ -135,7 +175,7 @@ getMarqueeMedia() {
 #   key2=another string
 #
 # Params: $1 = multiline string to search
-#		 $2 = key
+#         $2 = key
 # Output: e.g. "some string value"
 getValueForKey() {
 	#printf "DEBUG: getValueForKey('...', '%s')\n" "$2"
@@ -144,24 +184,6 @@ getValueForKey() {
 	# remove key= from start of line and return just the value
 	echo $(cut -f 2- -d'=' <<< "$value") || return 2
 }
-
-
-
-## debug
-#searchStr=$(cat <<END
-#ActionData=a very long string containing an = sign!
-#System=Speccy
-#END
-#)
-#echo "searchStr = $searchStr"
-
-#for key in "ActionData" "SystemId" "XXX"; do
-#	getValueForKey "$searchStr" "$key"
-#	echo "rc $?"
-#done
-
-#exit
-
 
 
 
@@ -188,8 +210,6 @@ while [[ "$actionData" != "quitrequested" ]]; do
 
 	# read event params from ES state file, stripping any CR characters
 	evParams=$(tr -d '\r' < $ES_STATE)
-	#echo "$evParams"
-
 	actionData=$(getValueForKey "$evParams" "ActionData")
 	systemId=$(getValueForKey "$evParams" "SystemId")
 	gamePath=$(getValueForKey "$evParams" "GamePath")
@@ -202,3 +222,7 @@ done < $FIFO
 
 # cleanup
 # trap SIGTERM here?
+
+# unsubscribe from MQTT topic
+mosquitto_sub -U Recalbox/EmulationStation/Event
+mosquitto_sub -E
