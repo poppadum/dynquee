b651ce5ba20f1c26a29bf3fa89dfe0992eda868a
Chris Laird <chris@pocketluff.net>
2023-01-22 19:59:50 +0000

    add MQTTSubscriber unit test
    test for clean exit if subscriber process dies unexpectedly

--- test/announce_time.sh
+++ test/announce_time.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-for i in {1..3}
+# Announce time repeatedly on stdout
+#
+# Options:
+# -c n: stop after n announcements (default 50)
+# -d x: wait x seconds between announcements (default 5s)
+
+
+# defaults:
+stopafter=50
+delay=5
+
+# read cmdline opts
+while(($#)) ; do
+    case "$1" in
+        "-c")
+            stopafter="$2"
+            shift; shift
+            ;;
+        "-d")
+            delay="$2"
+            shift; shift
+            ;;
+    esac
+done
+
+echo "Announcing time every ${delay}s, exiting after $stopafter announcements" >&2
+for i in $(seq 1 $stopafter)
 do
-    printf "the time is %s\n" $(date +'%H:%M:%S')
-    sleep 5
+    printf "the time is %s (%02d)\n" $(date +'%H:%M:%S') $i
+    sleep $delay
 done
 echo $0 end at $(date) >&2
--- test/test_digimarquee.py
+++ test/test_digimarquee.py
@@ -2,10 +2,12 @@
 
 # Unit tests for digimarquee module
 
-import unittest, os, logging
+import unittest, os, logging, threading, time
 from digimarquee import MQTTSubscriber, log
 
+# only log warnings and errors when running tests
 log.setLevel(logging.WARNING)
+
 
 class TestMQTTSubscriber(unittest.TestCase):
     '''unit tests for MQTTSubscriber'''
@@ -15,7 +17,7 @@ class TestMQTTSubscriber(unittest.TestCase):
         '''create a MQTTSubscriber instance; set paths for testing'''
         self.ms = MQTTSubscriber()
         self.ms._MQTT_CLIENT = '%s/announce_time.sh' % os.path.dirname(__file__)
-        self.ms._MQTT_CLIENT_OPTS = []
+        self.ms._MQTT_CLIENT_OPTS = ['-d', '5']
         self.ms._ES_STATE_FILE = '%s/es_state.inf' % os.path.dirname(__file__)
         return super(TestMQTTSubscriber, self).setUp()
     
@@ -66,5 +68,30 @@ class TestMQTTSubscriber(unittest.TestCase):
         # stop client
         self.ms.stop()
 
-    # TODO: add test that kills mock server and checks exits cleanly
 
+
+    def test_childKilled(self):
+        '''test that getEvent() exits cleanly if subscriber process is unexpectedly terminates'''
+        # thread to kill subscriber process after 17s
+        killer = threading.Thread(target = self._killChild, args=(17,))
+        self.ms.start()
+        killer.start()
+        # read events until child exits
+        count = 0
+        while True:
+            event = self.ms.getEvent()
+            if not event:
+                break
+            count += 1
+            print('event received: %s' % event)
+            self.assertTrue(event.startswith('the time is'))
+        self.assertEqual(count, 4)
+        self.assertIsNone(self.ms._childProcess)
+
+
+
+    def _killChild(self, delay):
+        '''Kill child process after the specified delay in seconds'''
+        time.sleep(delay)
+        print('killing child process now')
+        self.ms._childProcess.kill()
