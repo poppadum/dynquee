50f833176f3193c2c2c70d00835eb1079efc3a4b
Chris Laird <6032349+poppadum@users.noreply.github.com>
2023-02-13 16:55:23 +0000

    add runTests.sh options

--- runTests.sh
+++ runTests.sh
@@ -11,21 +11,35 @@ report() {
 }
 
 
-# static type checking
-echo "Running static type tests:"
-mypy *.py tests/*.py
-report $?
+unitTests() {
+    # static type checking
+    echo "Running static type tests:"
+    mypy *.py tests/*.py
+    report $?
 
-# unit tests
-echo "Running unit tests:"
-python3 -m unittest tests.test_dynquee
-report $?
+    # unit tests
+    echo "Running unit tests:"
+    python3 -m unittest tests.test_dynquee
+    report $?
+}
 
-# integration tests
-echo "Running EventHandler integration tests:"
-python3 -m tests.test_EventHandler
-report $?
+integrationTests() {
+    # integration tests
+    echo "Running EventHandler integration tests:"
+    python3 -m tests.test_EventHandler
+    report $?
 
-echo "Running Slideshow integration tests:"
-python3 -m tests.test_Slideshow
-report $?
+    echo "Running Slideshow integration tests:"
+    python3 -m tests.test_Slideshow
+    report $?
+}
+
+
+if [ "$1" == "-unit" ]; then
+    unitTests
+elif [ "$1" == "-int" ]; then
+    integrationTests
+else
+    unitTests
+    integrationTests
+fi
