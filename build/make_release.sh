adc1b0351beb279568c2b1f9e377f5d43f7a3911
Chris Laird <6032349+poppadum@users.noreply.github.com>
2023-02-21 21:40:05 +0000

    add VERSION file to release archives
    build script warns if working copy not clean
    include docs in release

--- build/make_release.sh
+++ build/make_release.sh
@@ -7,7 +7,7 @@
 build_dir=$(dirname "$0")
 
 # Location of created archive
-TARGET=$build_dir/dynquee-release.zip
+TARGET=$build_dir/dynquee.zip
 
 # list of files to include
 MANIFEST=$build_dir/MANIFEST
@@ -15,9 +15,21 @@ MANIFEST=$build_dir/MANIFEST
 # exclude my personal files in media/ and log files
 EXCLUDE='-x!media/generic/astrocade* -x!logs/*'
 
+# warn if working copy has uncommitted changes
+if ! git diff-index --quiet HEAD -- ; then
+    echo "Warning: working copy is not clean!"
+fi
+
+# generate temporary VERSION file to include in release
+VERSION_FILE=VERSION
+$build_dir/gen_version.sh > $build_dir/../$VERSION_FILE
+
 # build archive
 if [ -f "$TARGET" ]; then
     # remove existing archive if present
     rm $TARGET
 fi
-/usr/bin/7z a -tzip $EXCLUDE $TARGET @$MANIFEST
+/usr/bin/7z a -tzip -bd $EXCLUDE $TARGET @$MANIFEST $VERSION_FILE
+
+# remove temporary VERSION file
+rm $build_dir/../$VERSION_FILE
